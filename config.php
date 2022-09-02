3f013c66ddff8261c50a3c922aa4aa8adcf15aa1
Yen-Wen Chen <wenchen@letshack.us>
2022-09-02 21:00:09 +0800

    Use namespace to isolated similar plugin

--- config.php
+++ config.php
@@ -1,29 +1,25 @@
 <?php
+namespace GutenbergExTS {
+    const PROJECT_NAME = "gutenbergex-ts";
+    const PROJECT_CONFIG = array(
+        "name" => PROJECT_NAME,
 
-if ( ! defined( "ABSPATH" ) ) {
-    exit;
+        "client_name" => PROJECT_NAME."-client",
+        "admin_name" => PROJECT_NAME."-admin",
+        
+        "client_bundle_js_path" => "/public/client/main.js",
+        "client_bundle_css_path" => "/public/client/main.css",
+        "client_bundle_js_deps" => array( ),
+        "client_bundle_css_deps" => array( ),
+        "client_js_in_footer" => false,
+        "client_css_media" => "all",
+        "js_var_name" => "gex",
+        
+        "admin_bundle_js_path" => "/public/admin/main.js",
+        "admin_bundle_css_path" => "/public/admin/main.css",
+        "admin_bundle_js_deps" => array(  ),
+        "admin_bundle_css_deps" => array( ),
+        "admin_js_in_footer" => true,
+        "admin_css_media" => "all"
+    );
 }
-
-$project_name = "gutenberg-ts";
-$project_config = array(
-    "name" => $project_name,
-
-    "client_name" => "{$project_name}-client",
-    "admin_name" => "{$project_name}-admin",
-    
-    "client_bundle_js_path" => "/public/client/main.js",
-    "client_bundle_css_path" => "/public/client/main.css",
-    "client_bundle_js_deps" => array( ),
-    "client_bundle_css_deps" => array( ),
-    "client_js_in_footer" => false,
-    "client_css_media" => "all",
-    "js_var_name" => "privatefile",
-    
-    "admin_bundle_js_path" => "/public/admin/main.js",
-    "admin_bundle_css_path" => "/public/admin/main.css",
-    "admin_bundle_js_deps" => array(  ),
-    "admin_bundle_css_deps" => array( ),
-    "admin_js_in_footer" => true,
-    "admin_css_media" => "all"
-);
-
--- src/wp_enqueue.php
+++ src/wp_enqueue.php
@@ -1,4 +1,6 @@
 <?php
+namespace GutenbergExTS;
+
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
@@ -8,28 +10,26 @@ include_once(dirname(__DIR__).'/config.php');
 if (is_admin()) {
     add_action( 'admin_init', function() {
         add_action( 'enqueue_block_assets', function() {
-            global $project_config;
-
             wp_enqueue_script(
-                "{$project_config['admin_name']}-block-js",
-                plugins_url( $project_config['admin_bundle_js_path'], dirname( __FILE__ ) ),
-                $project_config['admin_bundle_js_deps'],
-                filemtime( dirname(__DIR__) . $project_config['admin_bundle_js_path'] ),
-                $project_config['admin_js_in_footer']
+                namespace\PROJECT_CONFIG['admin_name']."-block-js",
+                plugins_url( namespace\PROJECT_CONFIG['admin_bundle_js_path'], dirname( __FILE__ ) ),
+                namespace\PROJECT_CONFIG['admin_bundle_js_deps'],
+                filemtime( dirname(__DIR__) . namespace\PROJECT_CONFIG['admin_bundle_js_path'] ),
+                namespace\PROJECT_CONFIG['admin_js_in_footer']
             );
     
             wp_enqueue_style(
-                "{$project_config['admin_name']}-block-editor-css",
-                plugins_url( $project_config['admin_bundle_css_path'], dirname( __FILE__ ) ),
-                $project_config['admin_bundle_css_deps'],
-                filemtime( dirname(__DIR__) . $project_config['admin_bundle_css_path'] ),
-                $project_config['admin_css_media']
+                namespace\PROJECT_CONFIG['admin_name']."-block-editor-css",
+                plugins_url( namespace\PROJECT_CONFIG['admin_bundle_css_path'], dirname( __FILE__ ) ),
+                namespace\PROJECT_CONFIG['admin_bundle_css_deps'],
+                filemtime( dirname(__DIR__) . namespace\PROJECT_CONFIG['admin_bundle_css_path'] ),
+                namespace\PROJECT_CONFIG['admin_css_media']
             );
     
             register_block_type(
-                "{$project_config['name']}/admin-block", array(
-                    'editor_script' => "{$project_config['admin_name']}-block-js",
-                    'editor_style'  => "{$project_config['admin_name']}-block-editor-css",
+                namespace\PROJECT_CONFIG['name']."/admin-block", array(
+                    'editor_script' => namespace\PROJECT_CONFIG['admin_name']."-block-js",
+                    'editor_style'  => namespace\PROJECT_CONFIG['admin_name']."-block-editor-css",
                 )
             );
         });
@@ -37,38 +37,36 @@ if (is_admin()) {
 } else {
 
     add_action( 'init', function() {
-        global $project_config;
-
         wp_enqueue_script(
-            "{$project_config['client_name']}-js",
-            plugins_url( $project_config['client_bundle_js_path'], __FILE__ ),
-            $project_config['client_bundle_js_deps'],
-            filemtime( dirname(__DIR__) . $project_config['client_bundle_js_path'] ),
-            $project_config['client_js_in_footer']
+            namespace\PROJECT_CONFIG['client_name']."-js",
+            plugins_url( namespace\PROJECT_CONFIG['client_bundle_js_path'], __FILE__ ),
+            namespace\PROJECT_CONFIG['client_bundle_js_deps'],
+            filemtime( dirname(__DIR__) . namespace\PROJECT_CONFIG['client_bundle_js_path'] ),
+            namespace\PROJECT_CONFIG['client_js_in_footer']
         );
     
         wp_enqueue_style(
-            "{$project_config['client_name']}-style-css",
-            plugins_url( $project_config['client_bundle_css_path'], dirname( __FILE__ ) ),
-            $project_config['client_bundle_css_deps'],
-            filemtime( dirname(__DIR__) . $project_config['client_bundle_css_path'] ),
-            $project_config['client_css_media']
+            namespace\PROJECT_CONFIG['client_name']."-style-css",
+            plugins_url( namespace\PROJECT_CONFIG['client_bundle_css_path'], dirname( __FILE__ ) ),
+            namespace\PROJECT_CONFIG['client_bundle_css_deps'],
+            filemtime( dirname(__DIR__) . namespace\PROJECT_CONFIG['client_bundle_css_path'] ),
+            namespace\PROJECT_CONFIG['client_css_media']
         );
 
         add_action( 'wp', function() {
             include(dirname(__DIR__).'/variable.php');
 
             wp_add_inline_script(
-                "{$project_config['client_name']}",
-                "const {$project_config['js_var_name']} = ".json_encode($project_var),
+                namespace\PROJECT_CONFIG['client_name'],
+                "const ".namespace\PROJECT_CONFIG['js_var_name']." = ".json_encode($project_var),
                 'before'
             );
         });
     
         register_block_type(
-            "{$project_config['name']}/client-block", array(
-                'script' 		=> "{$project_config['client_name']}-js",
-                'style'         => "{$project_config['client_name']}-style-css",
+            namespace\PROJECT_CONFIG['name']."/client-block", array(
+                'script' 		=> namespace\PROJECT_CONFIG['client_name']."-js",
+                'style'         => namespace\PROJECT_CONFIG['client_name']."-style-css",
             )
         );
     });
